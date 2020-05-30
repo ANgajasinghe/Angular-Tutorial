4ab0e5b42f5b00f150661c34cd488c2b13b15c92
ANgajasinghe <angajasinghe@gmail.com>
2020-05-30 16:37:45 +0530

    init for Recipe app

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,14 +4,12 @@ import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+
   ],
   imports: [
     BrowserModule,
