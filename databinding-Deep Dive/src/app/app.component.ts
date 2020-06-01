fd0014b133a08b4e15b40d8153c9af5749878ccf
ANgajasinghe <angajasinghe@gmail.com>
2020-06-01 18:57:43 +0530

    <ng-content> this content holder allows you to add data to this from nother component

--- databinding-Deep Dive/src/app/app.component.ts
+++ databinding-Deep Dive/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  encapsulation:ViewEncapsulation.None // remove css encapsulation now this component css will works on others 
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   serverElements = [{type : 'server' , name : 'TestServer' , content : 'just a test!'}];
