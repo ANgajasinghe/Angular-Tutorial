d6d27e9d6801e57b763dfab27e9dba6bf431a8f5
ANgajasinghe <angajasinghe@gmail.com>
2020-06-01 11:04:26 +0530

    Component communication -01 @Input() //exposure attr to the world

---
+++ databinding-Deep Dive/src/app/server-element/server-element.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+
+  //exporse to the world 
+  @Input() element : {type : string, name:string , content: string}
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
