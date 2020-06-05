031cddd776f93aaef1f076236e327086ea517394
ANgajasinghe <angajasinghe@gmail.com>
2020-06-05 17:45:08 +0530

    Better way with out rendera

--- directives-Deep Dive/src/app/basic-highlight/better-h.directive.ts
+++ directives-Deep Dive/src/app/basic-highlight/better-h.directive.ts
@@ -1,27 +1,31 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterH]'
 })
 export class BetterHDirective implements OnInit{
   
+  @Input() defaultColor: string = 'transparent';
+  @Input('appBetterHighlight') highlightColor: string = 'blue';
   @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elRef : ElementRef , private renderer : Renderer2) { }
 
   //this is better way to access element
   ngOnInit(): void {
+    this.backgroundColor = this.defaultColor
     //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
 
   //reactive directive 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
-    //this.backgroundColor = this.defaultColor;
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = this.defaultColor;
   }
 
 }
