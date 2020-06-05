3c78611db7f937bb1e5334433cb653b5b44e5a2a
ANgajasinghe <angajasinghe@gmail.com>
2020-06-05 14:05:32 +0530

    Deep dive Directives - Recap

---
+++ directives-Deep Dive/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //numbers = [1, 2, 3, 4, 5];
+
+  oddNumber = [1,3,5];
+  evenNumber = [2,4]
+
+  onlyOdd = false;
+}
