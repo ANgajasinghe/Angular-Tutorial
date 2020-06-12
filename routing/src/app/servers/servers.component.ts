3f6c7e4f671aec31c00b328a27a4b95e7edc489c
ANgajasinghe <nayanajith@dmsswe.com>
2020-06-12 14:46:21 +0530

    Route : - Relative route

--- routing/src/app/servers/servers.component.ts
+++ routing/src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,20 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService , private router : Router , private currentRoute:ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onServerReload(){
+    /*
+      navigate function did not know where we are,so that it always act as globaly,
+
+      private currentRoute:ActivatedRoute) { }
+      SOLUTION : - {relativeTo:this.currentRoute}
+    */
+    this.router.navigate(['../servers'],{relativeTo:this.currentRoute});
   }
 
 }
