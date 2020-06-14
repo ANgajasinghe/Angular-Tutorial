b8ad18172a94661ee205769083a7d0847fd0538e
ANgajasinghe <angajasinghe@gmail.com>
2020-06-14 15:13:30 +0530

    Routing : - query param and fragments

--- routing/src/app/home/home.component.ts
+++ routing/src/app/home/home.component.ts
@@ -13,9 +13,12 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers(){
+  onLoadServers(id:number){
     // complex calculation 
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers',id,'edit'],{
+      queryParams : {allowEdit :'1'},
+      fragment : 'loading'}
+      );
 
   }
 
