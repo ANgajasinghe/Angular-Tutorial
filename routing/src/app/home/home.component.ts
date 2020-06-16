ad1033a58d6d183392dbca155ad95ec9e47ad873
ANgajasinghe <nayanajith@dmsswe.com>
2020-06-16 10:10:19 +0530

    Route : Implement login

--- routing/src/app/home/home.component.ts
+++ routing/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth-service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router : Router) { }
+  constructor(private router : Router , private authService : AuthService) { }
 
   ngOnInit() {
   }
@@ -20,6 +21,14 @@ export class HomeComponent implements OnInit {
       fragment : 'loading'}
       );
 
+  }
+
+  onLogIn(){
+    this.authService.login();
+  }
+
+  onLogout(){
+    this.authService.logOut();
   }
 
 }
