c12599854227d4e3e756a64164af23b7c137474b
ANgajasinghe <angajasinghe@gmail.com>
2020-06-21 11:49:01 +0530

    Observable : Pipe() and Operators

--- observable/src/app/home/home.component.ts
+++ observable/src/app/home/home.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { interval, Subscription, Observable } from 'rxjs';
-import { count } from 'console';
+import { map , filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit , OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  private firstObsDubcription : Subscription;
+  private firstObsDubcription: Subscription;
 
   constructor() { }
-  
+
 
   ngOnInit() {
 
@@ -28,36 +28,57 @@ export class HomeComponent implements OnInit , OnDestroy {
 
 
     // ---  TO AVOID MEMORY LEAK
-  //  this.firstObsDubcription = interval(2000).subscribe(count =>{
-  //                               console.log(count);
-  //                             });
+    //  this.firstObsDubcription = interval(2000).subscribe(count =>{
+    //                               console.log(count);
+    //                             });
 
-  //=========================Build Custome Obs
+    //=========================Build Custome Obs
 
     //observer : - we Talked this in the introduction 
-    const customIntervalObservable = Observable.create( observer =>{
+    const customIntervalObservable = Observable.create(observer => {
 
-      let count = 0;  
+      let count = 0;
 
-      setInterval(()=>{
-          observer.next(count);
-          if(count === 2){
-            observer.complete();
-          }
-          if (count > 3) {
-            observer.error(new Error('Count is grater than 3'));
-          }
-          count++;
-        },1000)
-      });
+      setInterval(() => {
+        observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is grater than 3'));
+        }
+        count++;
+      }, 1000)
+    });
 
-    this.firstObsDubcription = customIntervalObservable.subscribe(data => {
-                                  console.log(data);
-                                }, error => {
-                                  alert(error.message);
-                                },()=>{
-                                  console.log('Completed!');
-                                });
+
+    // customIntervalObservable.pipe(map((data : number)=> {
+    //     return `Rounde ${data + 1}`;
+    // }));
+
+    // this.firstObsDubcription = customIntervalObservable.subscribe(data => {
+    //                               console.log(data);
+    //                             }, error => {
+    //                               alert(error.message);
+    //                             },()=>{
+    //                               console.log('Completed!');
+    //                             });
+
+
+
+    this.firstObsDubcription = customIntervalObservable.pipe(
+      filter(data=>{
+        return data > 0
+      }),
+      map((data: number) => {
+      return `Rounde ${data + 1}`;
+    })).subscribe(data => {
+      console.log(data);
+    }, error => {
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
+    });
 
   }
 
