5f7f1461954a6571dfe324757edfe8eee2fe53b1
ANgajasinghe <angajasinghe@gmail.com>
2020-06-21 11:34:19 +0530

    Observable : Handle Observer  observer.next(count);  observer.complete();   observer.error(new Error('Count is grater than 3'));
       this.firstObsDubcription = customIntervalObservable.subscribe(data => {
                                      console.log(data);
                                    }, error => {
                                      alert(error.message);
                                    },()=>{
                                      console.log('Completed!');
                                    });

--- observable/src/app/home/home.component.ts
+++ observable/src/app/home/home.component.ts
@@ -36,16 +36,28 @@ export class HomeComponent implements OnInit , OnDestroy {
 
     //observer : - we Talked this in the introduction 
     const customIntervalObservable = Observable.create( observer =>{
+
       let count = 0;  
+
       setInterval(()=>{
           observer.next(count);
+          if(count === 2){
+            observer.complete();
+          }
+          if (count > 3) {
+            observer.error(new Error('Count is grater than 3'));
+          }
           count++;
         },1000)
-    });
+      });
 
     this.firstObsDubcription = customIntervalObservable.subscribe(data => {
-      console.log(data);
-    })
+                                  console.log(data);
+                                }, error => {
+                                  alert(error.message);
+                                },()=>{
+                                  console.log('Completed!');
+                                });
 
   }
 
