572d813a327744b9d863740aa61b8244b300973a
ANgajasinghe <angajasinghe@gmail.com>
2020-06-23 21:58:25 +0530

    Form : html form to angular object             local references             cast local references to agForm

--- forms-and-vaidations/src/app/app.component.ts
+++ forms-and-vaidations/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,7 @@ export class AppComponent {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(){
-    console.log('submitted')
+  onSubmit(form : NgForm){
+    console.log(form)
   }
 }
