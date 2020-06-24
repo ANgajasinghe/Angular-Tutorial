3fd8c36467e42930ada0bab00673ea311a668f83
ANgajasinghe <angajasinghe@gmail.com>
2020-06-24 18:40:26 +0530

    Form : Access form by @ViewChild
    This approach is very use full for validations

--- forms-and-vaidations/src/app/app.component.ts
+++ forms-and-vaidations/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,21 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f',{'static': true}) signupForm : NgForm;
+
+
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(form : NgForm){
-    console.log(form)
+  // onSubmit(form : NgForm){
+  //   console.log(form)
+  // }
+
+  onSubmit(){
+      console.log(this.signupForm);
   }
+
 }
