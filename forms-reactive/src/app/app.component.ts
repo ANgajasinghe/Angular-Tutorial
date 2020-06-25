e413d5188b51933e2168d92e7381922ada0f4b20
ANgajasinghe <angajasinghe@gmail.com>
2020-06-26 05:06:18 +0530

    Form : R Create Basic Form

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+
+  ngOnInit(): void {
+    // how to create a form
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email' : new FormControl(null),
+      'gender' : new FormControl('male')
+    });
+  }
 }
