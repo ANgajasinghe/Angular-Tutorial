f4a2917b35a76a34e60a610ec340fc1f316d9bc6
ANgajasinghe <angajasinghe@gmail.com>
2020-06-26 05:01:44 +0530

    Form : R setup importers

--- forms-reactive/src/app/app.module.ts
+++ forms-reactive/src/app/app.module.ts
@@ -11,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
