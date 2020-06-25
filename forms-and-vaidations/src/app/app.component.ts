5cfadd51d5bed20df718f1228bccdd65138c66e0
ANgajasinghe <angajasinghe@gmail.com>
2020-06-26 04:28:24 +0530

    Form : TD Fill Form Fields programmatically and Show and reset form

--- forms-and-vaidations/src/app/app.component.ts
+++ forms-and-vaidations/src/app/app.component.ts
@@ -13,11 +13,39 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male' , 'female'];
+  submitted = false;
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
 
 
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // ===================this is a oneway to access form elenemts (ONLY FOR ALL FIELD)
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender : this.genders[0]
+    // });
+
+    // =======better way (CAN USE PART OF VALUES )
+    this.signupForm.form.patchValue({
+      userData: {
+        username : suggestedName
+      }
+    });
+
+
   }
 
   // onSubmit(form : NgForm){
@@ -26,6 +54,13 @@ export class AppComponent {
 
   onSubmit() {
       console.log(this.signupForm);
+      this.submitted = true;
+      this.user.username = this.signupForm.value.userData.username;
+      this.user.email = this.signupForm.value.userData.email;
+      this.user.secretQuestion = this.signupForm.value.secret;
+      this.user.answer = this.signupForm.value.questionAnswer;
+      this.user.gender = this.signupForm.value.gender;
+      this.signupForm.reset();
   }
 
 }
