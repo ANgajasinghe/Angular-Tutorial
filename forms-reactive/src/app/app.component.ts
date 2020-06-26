f320a9dda7a32724f60b0f0b452fa24056616faa
ANgajasinghe <nayanajith@dmsswe.com>
2020-06-26 11:56:13 +0530

    Form : Custome Validator

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators, FormArray} from '@angular/forms';
+import {FormControl, FormGroup, Validators, FormArray, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +10,17 @@ export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
 
+  forbiddenUsernames = ['Chris','Anna'];
+
+
+
+  constructor(private formBuilder : FormBuilder){}
+
   ngOnInit(): void {
     // how to create a form
     this.signupForm = new FormGroup({
       'userData' : new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null,[ Validators.required , this.forbiddenNames.bind(this)]),
         'email' : new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender' : new FormControl('male'),
@@ -30,5 +36,24 @@ export class AppComponent implements OnInit {
   onAddHobby(){
     const control = new FormControl(null,Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control)
+  }
+
+
+  forbiddenNames(control: FormControl) : {[s:string]:boolean}{
+    // ===== this part will execute when match the usernames=====
+    // if(this.forbiddenUsernames.indexOf(control.value)){
+    //   return {'isValidUser':true};
+    // } else {
+    //   return null; // ** only return null or omit the return statement
+    // }
+
+    // ===== this part will execute when usernames dosen't matches=====
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1){
+      return {'isValidUser':true};
+    } else {
+      return null; // ** only return null or omit the return statement
+    }
+
+
   }
 }
