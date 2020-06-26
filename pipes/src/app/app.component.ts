d3b0114d1a48a9963045512791c3325f1e71ddce
ANgajasinghe <nayanajith@dmsswe.com>
2020-06-26 16:57:01 +0530

    Pipe : async pipe

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  appStatus = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+
   servers = [
     {
       instanceType: 'medium',
