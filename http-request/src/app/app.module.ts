154b97422ac7af5f8816c383ab79fba863180d00
ANgajasinghe <angajasinghe@gmail.com>
2020-06-27 11:10:32 +0530

    Http : Setting HTTP_INTERCEPTORS (important)

--- http-request/src/app/app.module.ts
+++ http-request/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {AuthInterceptorService} from './auth-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptorService,
+    multi:true // to all
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ http-request/src/app/auth-interceptor.service.ts
@@ -0,0 +1,22 @@
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export class AuthInterceptorService implements HttpInterceptor{
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    // this class will implement with every request send by this app
+    // we can set headers and manipulate some very important data through this service
+    // you need to configure app.module ts => provider section as follows
+    /*
+    *  providers: [{
+        provide: HTTP_INTERCEPTORS,
+        useClass: AuthInterceptorService,
+        multi:true // to all
+      }],
+    *
+    * */
+    console.log('Request is on the way');
+    return next.handle(req);
+  }
+
+}
