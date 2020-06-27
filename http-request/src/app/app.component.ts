052865552a529f466c08d6770190ca805b0331f5
ANgajasinghe <angajasinghe@gmail.com>
2020-06-27 08:29:09 +0530

    Http : make http GET and add map operator pipe,cast response type , create a model

--- http-request/src/app/app.component.ts
+++ http-request/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {PostModel} from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +13,15 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: PostModel) {
     // posts.json -> this is only a firebase requirement
     // postData will transform to JSON by angular
 
-    this.http.post('https://ng-http-18a69.firebaseio.com/posts.json', postData).subscribe(responseData => {
+    this.http.post<{name : string}>('https://ng-http-18a69.firebaseio.com/posts.json', postData).subscribe(responseData => {
       console.log(responseData);
     });
     console.log(postData);
@@ -29,5 +33,23 @@ export class AppComponent implements OnInit {
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get< {[key:string]:PostModel}>('https://ng-http-18a69.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postArray : PostModel[] = [];
+
+        for (const key in responseData ) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({...responseData[key], id:key});
+          }
+
+        }
+        return postArray;
+      }))
+      .subscribe(results => {
+      console.log(results);
+    });
   }
 }
---
+++ http-request/src/app/post.model.ts
@@ -0,0 +1,5 @@
+export interface PostModel {
+  title : string;
+  content : string;
+  id?: string ;// this is a key but it is optional
+}
